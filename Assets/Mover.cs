32454c7a867a924383c7eebc7fe8b6c71c3f619d
Cameron Helkuik <cameronhelkuik@gmail.com>
2021-12-07 20:16:46 -0600

    change wall color on collide

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -7,10 +7,22 @@ public class Mover : MonoBehaviour
     [SerializeField] float moveSpeed = 10f;
     void Start()
     {
-
+        PrintInstructions();
     }
 
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game.");
+        Debug.Log("Do your best to make it through the maze");
+        Debug.Log("Move your player with WASD or arrow keys");
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
